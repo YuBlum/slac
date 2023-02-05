4ab58158098b500e371360cc89beb1f5771d4164
blum <yuuybonito@gmail.com>
2023-02-05 02:03:23 -0300

    vectors with any amount of members

--- test/main.c
+++ test/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#define __USING_SLAC__
 #include <slac/vector.h>
 
 int
 main(void) {
-	vector a = vector(2);
-	printf("%u\n", vector_size(a));
+	vector a = vector_add(2, vector(2, 2, 4), vector_mul(2, vector(2, 1, 2), vector(2, 2, 2)));
+	printf("%.1f, %.1f\n", a[0], a[1]);
 	return 0;
 }
